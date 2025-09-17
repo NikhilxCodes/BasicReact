799ba53f9c90a7e0bab4df83d7b36cc550b119f8
Nikhil <nikhil18sagar@gmail.com>
2025-09-18 03:53:37 +0530

    edited

--- Forms/src/App.jsx
+++ Forms/src/App.jsx
@@ -1,19 +1,50 @@
+import React, { useState } from 'react'
+import Cards from './components/Cards'
+import Form from './components/Form'
 
-// 3rd way using react-hook-form library to handle form submission
-import React from 'react'
-import { useForm } from 'react-hook-form';
 const App = () => {
-  const {register,handleSubmit}=useForm();
+  const [users,setUsers]=useState([]);
+  const handleFormSubmit=(data)=>{
+    setUsers([...users,data])
+  }
+  const handleRemove=(id)=>{
+    setUsers(()=>users.filter((item,index)=>index!==id))
+  }
   return (
-    <form action="" onSubmit={handleSubmit(data=>console.log(data))}>
-      <input {...register('name')} placeholder='name' type="text" className='border-2'/>
-      <input {...register('email')} placeholder='email' type="email" className='border-2'/>
-      <input type="submit" />
-    </form>
+    <div className='w-full h-screen bg-zinc-200 flex justify-center items-center'>
+      <div className='container mx-auto'>
+        <Cards handleRemove={handleRemove} users={users} />
+        <Form handleFormSubmit={handleFormSubmit}/>
+      </div>
+    </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+// 3rd way using react-hook-form library to handle form submission
+// import React from 'react'
+// import { useForm } from 'react-hook-form';
+// const App = () => {
+//   const {register,handleSubmit}=useForm();
+//   return (
+//     <form action="" onSubmit={handleSubmit(data=>console.log(data))}>
+//       <input {...register('name')} placeholder='name' type="text" className='border-2'/>
+//       <input {...register('email')} placeholder='email' type="email" className='border-2'/>
+//       <input type="submit" />
+//     </form>
+//   )
+// }
+
+// export default App
 
 
 
